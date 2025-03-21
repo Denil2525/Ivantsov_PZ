6058289c3ee7fd28e7b5846967f5b3fa819dcc27
BoxWolf2525 <108700207+Denil2525@users.noreply.github.com>
2025-03-21 14:29:33 +0300

    Add files via upload

--- PZ-11/PZ-11.1.py
+++ PZ-11/PZ-11.1.py
@@ -1,25 +1,27 @@
-l = ['-99 6 12 -36 20 45 100 -15']
+l = ['1 -1 2 -2 3 -3 4 -4 5 -5']
 f3 = open('data_3.txt', 'w')
 f3.writelines(l)
 f3.close()
 f4 = open('data_4.txt', 'w')
 f4.write('Исходные данные: ')
-f4.write('\n')
 f4.writelines(l)
 f4.close()
 f3 = open('data_3.txt')
 k = f3.read()
 k = k.split()
+n = k
+n.sort(key=None, reverse=True)
+inmax = n
 for i in range(len(k)):
     k[i] = int(k[i])
 f3.close()
 f3 = open('data_3.txt')
-max, t = 0, 0
+inmax, t = 0, 0
 for i in range(len(k)):
-    max = max if max > k[i] else k[i]
     if k[i] < 0:
         t += 1
 f4 = open('data_4.txt', 'a')
 f4.write('\n')
-print('Количество элементов:', len(k), 'Максимальный элемент:', max, file=f4)
+print('Количество элементов:', len(k), file=f4)
+print('Индекс последнего максимального элемента:', inmax, file=f4)
 f4.close()
