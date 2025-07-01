630b39bf3cb7d47193ce057651ea5ec8ae99898d
BoxWolf2525 <108700207+Denil2525@users.noreply.github.com>
2025-07-01 09:00:48 +0300

    Add files via upload

--- PZ-13/Pz-13.1.py
+++ PZ-13/Pz-13.1.py
@@ -1,18 +1,18 @@
 #В двумерном списке элементы второго столбца заменить элементами из
 #одномерного динамического массива соответствующей размерности.
+def replace_col(matrix, col_index, new_value):
+    return[
+        [row[col] if col != col_index else new_value[i]
+         for col in range(len(row))]
+         for i, row in enumerate(matrix)
+]
 matrix = [
     [1, 2, 3],
     [4, 5, 6],
     [7, 8, 9],
 ]
-new_col = [10, 20, 30]
-k = 1
-
-if len(new_col) == len(matrix) and len(matrix[0]) > k:
-    for i in range(len(matrix)):
-        matrix[i][k] = new_col[i]
-else: print("Ошибка")
-
-print(matrix[0])
-print(matrix[1])
-print(matrix[2])
+new = [10, 20, 30]
+new_matrix = replace_col(matrix, 1, new)
+print(new_matrix[0])
+print(new_matrix[1])
+print(new_matrix[2])
--- PZ-13/Pz-13.2.py
+++ PZ-13/Pz-13.2.py
@@ -1,18 +1,22 @@
+#В двумерном списке найти среднее арифметическое положительных элементов,
+#кратных 3
+from functools import reduce
+def cal_SA(matrix):
+    filtered = [
+        element
+        for row in matrix
+        for element in row
+        if element > 0 and element % 3 == 0
+    ]
+    if not filtered:
+        return None
+    return reduce(lambda x, y: x + y, filtered) / len(filtered)
+
 matrix = [
     [1, 2, 3],
     [4, 5, 6],
     [7, 8, 9],
 ]
-sum = 0
-count = 0
+SA = cal_SA(matrix)
+print(f"Среднее арифметическое {SA}")
 
-for row in matrix:
-    for element in row:
-        if element > 0 and element % 3 == 0:
-            sum += element
-            count += 1
-if count > 0:
-    SA = sum / count
-    print(f"Среднее арифметическое {SA}")
-else:
-    print("Нет подходящих элементов")
